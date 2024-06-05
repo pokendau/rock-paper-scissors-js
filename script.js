01f94633d72707faa2142bab00fe0dac7e0634ff
daniel <daniplingau@gmail.com>
2024-06-05 09:57:35 +0300

    Js logic for buttons works

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const choices = ["rock", "paper", "scissors"];
 
+const scissorBtn = document.getElementById("scissor_btn");
+const rockBtn = document.getElementById("rock_btn");
+const paperBtn = document.getElementById("paper_btn");
+
+const player = document.getElementById("player");
+const computer = document.getElementById("computer");
+
+console.log(`${player}, ${computer}`);
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -34,4 +43,77 @@ function playRound(humanChoice, computerChoice) {
     );
     computerScore++;
   }
+}
+
+scissorBtn.addEventListener("click", (ev) => {
+  onBtnClick("scissors");
+});
+
+rockBtn.addEventListener("click", (ev) => {
+  onBtnClick("rock");
+});
+
+paperBtn.addEventListener("click", (ev) => {
+  onBtnClick("paper");
+});
+
+function onBtnClick(playerChoice) {
+  player.children[0].src = "./images/fist.png";
+  computer.children[0].src = "./images/fist.png";
+
+  scissorBtn.disabled = true;
+  rockBtn.disabled = true;
+  paperBtn.disabled = true;
+
+  const computerChoice = getComputerChoice();
+
+  toggleClass(player, "animated");
+  toggleClass(computer, "animated2");
+
+  setTimeout(() => {
+    toggleClass(player, "animated");
+    toggleClass(computer, "animated2");
+
+    player.children[0].src = `./images/${playerChoice}.png`;
+    computer.children[0].src = `./images/${computerChoice}.png`;
+
+    const winner = checkWinner(playerChoice, computerChoice);
+    console.log(winner);
+  }, 950);
+
+  setTimeout(() => {
+    scissorBtn.disabled = false;
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+  }, 1000);
+}
+
+function toggleClass(element, className) {
+  element.classList.toggle(className);
+}
+
+function checkWinner(playerChoice, computerChoice) {
+  const txt = document.getElementById("screen__text");
+  if (playerChoice == computerChoice) {
+    txt.innerHTML = "It is a tie 🫤";
+    txt.style.color = "yellow";
+  } else if (
+    (playerChoice == "rock" && computerChoice == "scissors") ||
+    (playerChoice == "paper" && computerChoice == "rock") ||
+    (playerChoice == "scissors" && computerChoice == "paper")
+  ) {
+    humanScore++;
+    txt.innerHTML = "You won 🥳";
+    txt.style.color = "limegreen";
+  } else {
+    computerScore++;
+    txt.innerHTML = "Computer won 😤";
+    txt.style.color = "red";
+  }
+  changeScore();
+}
+
+function changeScore() {
+  document.getElementById("player__score").innerHTML = humanScore;
+  document.getElementById("computer__score").innerHTML = computerScore;
 }
